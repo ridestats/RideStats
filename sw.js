d0d2cf98f9bb2463a1a2af67d77aba6d4011dd7c
ridestats <marius.stefanache@gmail.com>
2026-04-20 22:44:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,6 @@
 const CACHE = 'ridestats-v4';
-const ASSETS = [
-  '/RideStats/',
-  '/RideStats/index.html',
-  '/RideStats/manifest.json',
-  '/RideStats/logo.png'
-];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).then(res=>{
-    const clone=res.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,clone));
-    return res;
-  }).catch(()=>caches.match(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
